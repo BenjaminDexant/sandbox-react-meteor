b6fb1cdaca90dc20eea5748bc1083408231d3e4a
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-09-28 15:35:18 +0200

    Step 4 : Update & Remove

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import { useTracker } from 'meteor/react-meteor-data';
-import { TasksCollection } from '/imports/api/TasksCollection';
-import { Task } from './components/Task';
-import { TaskForm } from './components/TaskForm';
- 
+import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { Task } from "./components/Task";
+import { TaskForm } from "./components/TaskForm";
+
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+	const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
-  return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
+	const toggleChecked = ({ _id, isChecked }) => {
+		TasksCollection.update(_id, {
+			$set: {
+				isChecked: !isChecked,
+			},
+		});
+	};
 
-      <TaskForm />
+  const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
-      <ul>
-        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Welcome to Meteor!</h1>
+			<TaskForm />
+			<ul>
+				{tasks.map((task) => (
+					<Task
+						key={task._id}
+						task={task}
+						onCheckboxClick={toggleChecked}
+						onDeleteClick={deleteTask}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 };
